68326ca94087c970c7ecbacf6f27e2b5f4a0aa65
alirezabeygijelva <alirezabeygi840@gmail.com>
2025-06-12 15:55:13 +0330

    remove unnecessary test dependencies

--- company-lookup/build.gradle.kts
+++ company-lookup/build.gradle.kts
@@ -6,7 +6,5 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework:spring-context")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
