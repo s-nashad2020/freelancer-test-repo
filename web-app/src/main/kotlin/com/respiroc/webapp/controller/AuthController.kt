fa7ab86a55a247a03456f7c54ea45b55345c2ed5
alirezabeygijelva <alirezabeygi840@gmail.com>
2025-06-18 14:30:18 +0300

    Add login endpoint

--- web-app/src/main/kotlin/com/respiroc/webapp/controller/AuthController.kt
+++ web-app/src/main/kotlin/com/respiroc/webapp/controller/AuthController.kt
@@ -1,7 +1,9 @@
 package com.respiroc.webapp.controller
 
 import com.respiroc.user.api.UserInternalApi
+import com.respiroc.webapp.controller.request.LoginRequest
 import com.respiroc.webapp.controller.request.SignupRequest
+import com.respiroc.webapp.controller.response.LoginResponse
 import com.respiroc.webapp.controller.response.SignupResponse
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -13,11 +15,19 @@ class AuthController(
 ) : BaseController() {
 
     @PostMapping(value = ["/signup"])
-    fun signup(@RequestBody signupRequest: SignupRequest): ResponseEntity<SignupResponse> {
-        if (signupRequest.password != signupRequest.confirmPassword) {
+    fun signup(@RequestBody request: SignupRequest): ResponseEntity<SignupResponse> {
+        // TODO: Validate request data
+        if (request.password != request.confirmPassword) {
             throw IllegalArgumentException("Passwords don't match")
         }
-        val result = userApi.signupByEmailPassword(signupRequest.email, signupRequest.password)
+        val result = userApi.signupByEmailPassword(request.email, request.password)
         return ResponseEntity.ok(SignupResponse(result.token))
+    }
+
+    @PostMapping(value = ["/login"])
+    fun login(@RequestBody request: LoginRequest): ResponseEntity<LoginResponse> {
+        // TODO: Validate request data
+        val result = userApi.loginByEmailPassword(email = request.email, password = request.password)
+        return ResponseEntity.ok(LoginResponse(result.token))
     }
 } 
