2b7d90863362256e89fb230a8a6fa1f9e15a157f
alirezabeygijelva <alirezabeygi840@gmail.com>
2025-06-18 09:13:39 +0300

    Update user internal api

--- user/src/main/kotlin/com/respiroc/user/api/UserInternalApi.kt
+++ user/src/main/kotlin/com/respiroc/user/api/UserInternalApi.kt
@@ -1,16 +1,19 @@
 package com.respiroc.user.api
 
+import com.respiroc.user.api.result.ForgotResult
+import com.respiroc.user.api.result.LoginResult
+import com.respiroc.user.api.result.SignupResult
 import com.respiroc.util.dto.UserContext
 
 interface UserInternalApi {
-    fun signup()
-    fun loginByUsernamePassword()
-    fun changePassword()
-    fun logout()
-    fun forgetPassword()
-    fun resetPassword()
-    fun setPassword()
+    fun signup(email: String, password: String): SignupResult
+    fun loginByEmailPassword(email: String, password: String): LoginResult
+    fun changePassword(user: UserContext, oldPassword: String, newPassword: String)
+    fun logout(token: String)
+    fun forgetPassword(email: String, ipAddress: String): ForgotResult
+    fun resetPassword(token: String, newPassword: String)
+    fun setPassword(user: UserContext, newPassword: String)
     fun findByToken(token: String): UserContext?
-    fun findByEmail()
-    fun generateToken()
+    fun findByEmail(email: String): UserContext?
+    fun generateToken(user: UserContext): String
 } 
--- user/src/main/kotlin/com/respiroc/user/application/UserService.kt
+++ user/src/main/kotlin/com/respiroc/user/application/UserService.kt
@@ -1,6 +1,9 @@
 package com.respiroc.user.application
 
 import com.respiroc.user.api.UserInternalApi
+import com.respiroc.user.api.result.ForgotResult
+import com.respiroc.user.api.result.LoginResult
+import com.respiroc.user.api.result.SignupResult
 import com.respiroc.util.dto.UserContext
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
@@ -16,31 +19,41 @@ class UserService() : UserInternalApi {
     private val base64Encoder: Base64.Encoder = Base64.getUrlEncoder()
     private val passwordEncoder = BCryptPasswordEncoder()
 
-    override fun signup() {
+    override fun signup(email: String, password: String): SignupResult {
         TODO("Not yet implemented")
     }
 
-    override fun loginByUsernamePassword() {
+    override fun loginByEmailPassword(
+        email: String,
+        password: String
+    ): LoginResult {
         TODO("Not yet implemented")
     }
 
-    override fun changePassword() {
+    override fun changePassword(
+        user: UserContext,
+        oldPassword: String,
+        newPassword: String
+    ) {
         TODO("Not yet implemented")
     }
 
-    override fun logout() {
+    override fun logout(token: String) {
         TODO("Not yet implemented")
     }
 
-    override fun forgetPassword() {
+    override fun forgetPassword(
+        email: String,
+        ipAddress: String
+    ): ForgotResult {
         TODO("Not yet implemented")
     }
 
-    override fun resetPassword() {
+    override fun resetPassword(token: String, newPassword: String) {
         TODO("Not yet implemented")
     }
 
-    override fun setPassword() {
+    override fun setPassword(user: UserContext, newPassword: String) {
         TODO("Not yet implemented")
     }
 
@@ -48,11 +61,11 @@ class UserService() : UserInternalApi {
         TODO("Not yet implemented")
     }
 
-    override fun findByEmail() {
+    override fun findByEmail(email: String): UserContext? {
         TODO("Not yet implemented")
     }
 
-    override fun generateToken() {
+    override fun generateToken(user: UserContext): String {
         TODO("Not yet implemented")
     }
 }
