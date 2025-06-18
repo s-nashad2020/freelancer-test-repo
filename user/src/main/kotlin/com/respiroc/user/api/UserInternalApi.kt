6bd1f66bb97ae3bc49f72c0621132ea6c753b16c
alirezabeygijelva <alirezabeygi840@gmail.com>
2025-06-18 14:05:55 +0300

    Update user internal api and result DTOs

--- user/src/main/kotlin/com/respiroc/user/api/UserInternalApi.kt
+++ user/src/main/kotlin/com/respiroc/user/api/UserInternalApi.kt
@@ -3,17 +3,18 @@ package com.respiroc.user.api
 import com.respiroc.user.api.result.ForgotResult
 import com.respiroc.user.api.result.LoginResult
 import com.respiroc.user.api.result.SignupResult
+import com.respiroc.util.dto.SpringUser
 import com.respiroc.util.dto.UserContext
 
 interface UserInternalApi {
-    fun signup(email: String, password: String): SignupResult
+    fun signupByEmailPassword(email: String, password: String): SignupResult
     fun loginByEmailPassword(email: String, password: String): LoginResult
-    fun changePassword(user: UserContext, oldPassword: String, newPassword: String)
+    fun changePassword(user: SpringUser, oldPassword: String, newPassword: String)
     fun logout(token: String)
     fun forgetPassword(email: String, ipAddress: String): ForgotResult
     fun resetPassword(token: String, newPassword: String)
-    fun setPassword(user: UserContext, newPassword: String)
+    fun setPassword(user: SpringUser, newPassword: String)
     fun findByToken(token: String): UserContext?
     fun findByEmail(email: String): UserContext?
-    fun generateToken(user: UserContext): String
+    fun generateToken(user: SpringUser): String
 } 
--- user/src/main/kotlin/com/respiroc/user/api/result/SignupResult.kt
+++ user/src/main/kotlin/com/respiroc/user/api/result/SignupResult.kt
@@ -1,5 +1,5 @@
 package com.respiroc.user.api.result
 
-import com.respiroc.util.dto.UserContext
+import com.respiroc.util.dto.SpringUser
 
-data class SignupResult(val token: String, val user: UserContext)
+data class SignupResult(val token: String, val user: SpringUser)
