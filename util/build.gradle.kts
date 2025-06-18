3a2d332623d3eab5110af22bbeb2b030ab147129
alirezabeygijelva <alirezabeygi840@gmail.com>
2025-06-18 09:02:41 +0300

    Add spring jpa starter to util module

--- util/build.gradle.kts
+++ util/build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-database-postgresql")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("org.postgresql:postgresql")
 }
