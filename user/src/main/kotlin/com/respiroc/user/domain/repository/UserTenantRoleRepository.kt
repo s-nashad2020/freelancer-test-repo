4e027a96a850cf124411b31ec5b6a7d0d5b6338d
alirezabeygijelva <alirezabeygi840@gmail.com>
2025-06-19 11:08:24 +0300

    Add user tenant role repository in user module

---
+++ user/src/main/kotlin/com/respiroc/user/domain/repository/UserTenantRoleRepository.kt
@@ -0,0 +1,9 @@
+package com.respiroc.user.domain.repository
+
+import com.respiroc.user.domain.model.UserTenantRole
+import com.respiroc.util.repository.CustomJpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserTenantRoleRepository : CustomJpaRepository<UserTenantRole, Long> {
+}
