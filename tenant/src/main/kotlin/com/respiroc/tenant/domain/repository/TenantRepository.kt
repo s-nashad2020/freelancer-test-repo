64217e9881437a1475c90fa865d4aef1625d3283
alirezabeygijelva <alirezabeygi840@gmail.com>
2025-06-19 10:55:01 +0300

    Add tenant module jpa repositories

---
+++ tenant/src/main/kotlin/com/respiroc/tenant/domain/repository/TenantRepository.kt
@@ -0,0 +1,9 @@
+package com.respiroc.tenant.domain.repository
+
+import com.respiroc.tenant.domain.model.Tenant
+import com.respiroc.util.repository.CustomJpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface TenantRepository : CustomJpaRepository<Tenant, Long> {
+}
---
+++ tenant/src/main/kotlin/com/respiroc/tenant/domain/repository/TenantRoleRepository.kt
@@ -0,0 +1,9 @@
+package com.respiroc.tenant.domain.repository
+
+import com.respiroc.tenant.domain.model.TenantRole
+import com.respiroc.util.repository.CustomJpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface TenantRoleRepository : CustomJpaRepository<TenantRole, Long> {
+}
