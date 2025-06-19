354b1926c0a448a5e06417e99314b987d56b3d1e
alirezabeygijelva <alirezabeygi840@gmail.com>
2025-06-19 11:11:23 +0300

    Renamed CompanyController to CompanyLookupController

--- web-app/src/main/kotlin/com/respiroc/webapp/controller/CompanyLookupController.kt
+++ web-app/src/main/kotlin/com/respiroc/webapp/controller/CompanyLookupController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/companies")
-class CompanyController(
+class CompanyLookupController(
     private val companyLookupApi: CompanyLookupInternalApi
 ) {
 
