99353309f6aec4cb2fb4b547c00a883ab582b240
alirezabeygijelva <alirezabeygi840@gmail.com>
2025-06-19 10:19:08 +0300

    Add user tenant role entity to user module

---
+++ user/src/main/kotlin/com/respiroc/user/domain/model/UserTenantRole.kt
@@ -0,0 +1,38 @@
+package com.respiroc.user.domain.model
+
+import com.respiroc.tenant.domain.model.Tenant
+import com.respiroc.tenant.domain.model.TenantRole
+import jakarta.persistence.*
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.time.Instant
+
+@Entity
+@Table(name = "user_tenant_roles")
+open class UserTenantRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "record_id", nullable = false)
+    open var id: Long = -1
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_record_id", nullable = false)
+    open lateinit var user: User
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "tenant_record_id", nullable = false)
+    open lateinit var tenant: Tenant
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "tenant_role_record_id", nullable = false)
+    open lateinit var tenantRole: TenantRole
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    open lateinit var createdAt: Instant
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    open lateinit var updatedAt: Instant
+}
