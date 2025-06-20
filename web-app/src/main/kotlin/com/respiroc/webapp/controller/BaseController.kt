9d1a41fd7efbdb91fef4899dc4435ad7819f7056
alirezabeygijelva <alirezabeygi840@gmail.com>
2025-06-20 08:08:29 +0330

    Remove json method from BaseController

--- web-app/src/main/kotlin/com/respiroc/webapp/controller/BaseController.kt
+++ web-app/src/main/kotlin/com/respiroc/webapp/controller/BaseController.kt
@@ -1,20 +1,10 @@
 package com.respiroc.webapp.controller
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.respiroc.util.context.UserContext
 import com.respiroc.util.context.SpringUser
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.context.SecurityContextHolder
 
 open class BaseController {
-
-    @Autowired
-    lateinit var objectMapper: ObjectMapper
-
-    fun json(obj: Any?): String? {
-        if (obj == null) return null
-        return objectMapper.writeValueAsString(obj)
-    }
 
     fun springUser(): SpringUser {
         return SecurityContextHolder.getContext().authentication.principal as SpringUser
