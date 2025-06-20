4ac6fddbf652c018a425a9442b69ceb287ece58c
alirezabeygijelva <alirezabeygi840@gmail.com>
2025-06-20 13:27:50 +0330

    Add get all companies endpoint controller

--- web-app/src/main/kotlin/com/respiroc/webapp/controller/CompanyController.kt
+++ web-app/src/main/kotlin/com/respiroc/webapp/controller/CompanyController.kt
@@ -5,6 +5,7 @@ import com.respiroc.company.api.command.CreateCompanyCommand
 import com.respiroc.company.domain.model.Company
 import com.respiroc.webapp.controller.request.CreateCompanyRequest
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -19,6 +20,12 @@ class CompanyController(private val companyApi: CompanyInternalApi) : BaseContro
         val result = companyApi.createNewCompany(
             CreateCompanyCommand(request.name, request.organizationNumber, request.countryCode),
             user())
+        return ResponseEntity.ok(result)
+    }
+
+    @GetMapping(value = [""])
+    fun getAllCompany(): ResponseEntity<List<Company>> {
+        val result = companyApi.findAllCompanyByUser(user())
         return ResponseEntity.ok(result)
     }
 }
