6a75a0978834c6a28dbf6e87af503ec54cf57004
Alireza Beygi <94733363+alirezabeygijelva@users.noreply.github.com>
2025-06-24 11:47:04 +0300

    Add ledger module

---
+++ ledger/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("org.springframework.boot")
+}
+
+dependencies {
+    implementation(project(":util"))
+
+    implementation("org.springframework:spring-context")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    runtimeOnly("org.postgresql:postgresql")
+}
