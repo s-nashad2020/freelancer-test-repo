6e60b45a690d471506e61a0f3786b26e795c7d32
Rohi <roohinofrsti@gmail.com>
2025-07-01 16:26:06 +0400

    adding addCommonAttributes function for pages common data

--- web-app/src/main/kotlin/com/respiroc/webapp/controller/BaseController.kt
+++ web-app/src/main/kotlin/com/respiroc/webapp/controller/BaseController.kt
@@ -1,10 +1,17 @@
 package com.respiroc.webapp.controller
 
-import com.respiroc.util.context.UserContext
+import com.respiroc.company.api.CompanyInternalApi
 import com.respiroc.util.context.SpringUser
+import com.respiroc.util.context.UserContext
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.ui.Model
 
-open class BaseController {
+open class BaseController() {
+
+    private val titleAttributeName: String = "title"
+    private val userAttributeName: String = "user"
+    private val companiesAttributeNames: String = "companies"
+    private val currentCompanyAttributeNames: String = "currentCompany"
 
     fun springUser(): SpringUser {
         return SecurityContextHolder.getContext().authentication.principal as SpringUser
@@ -12,5 +19,20 @@ open class BaseController {
 
     fun user(): UserContext {
         return springUser().ctx
+    }
+
+    open fun addCommonAttributes(model: Model, title: String) {
+        model.addAttribute(titleAttributeName, title)
+    }
+
+    fun addCommonAttributes(model: Model, companyApi: CompanyInternalApi, title: String) {
+        addCommonAttributes(model, title)
+        val springUser = springUser()
+        val tenantId = springUser.ctx.currentTenant!!.id
+        model.addAttribute(userAttributeName, springUser)
+        val companies = companyApi.findAllCompanyByUser(springUser.ctx)
+        model.addAttribute(companiesAttributeNames, companies)
+        val currentCompany = companies.find { it.tenantId == tenantId }
+        model.addAttribute(currentCompanyAttributeNames, currentCompany)
     }
 }
