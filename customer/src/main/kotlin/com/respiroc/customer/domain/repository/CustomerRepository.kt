08fc81b9010734480664db528e8588212ccc15a5
Rohi <roohinofrsti@gmail.com>
2025-07-02 12:25:52 +0400

    use multiline string

--- customer/src/main/kotlin/com/respiroc/customer/domain/repository/CustomerRepository.kt
+++ customer/src/main/kotlin/com/respiroc/customer/domain/repository/CustomerRepository.kt
@@ -10,17 +10,21 @@ import org.springframework.stereotype.Repository
 interface CustomerRepository : CustomJpaRepository<Customer, Long> {
 
     @Query(
-        "SELECT customer FROM Customer customer \n" +
-                "LEFT JOIN FETCH customer.tenant \n" +
-                "WHERE customer.tenantId = :tenantId"
+        """
+        SELECT customer FROM Customer customer
+                LEFT JOIN FETCH customer.tenant 
+                WHERE customer.tenantId = :tenantId
+                """
     )
     fun findAllByTenantId(@Param("tenantId") tenantId: Long): List<Customer>;
 
     @Query(
-        "SELECT customer FROM Customer customer \n" +
-                "LEFT JOIN FETCH customer.tenant \n" +
-                "WHERE customer.tenantId = :tenantId \n" +
-                "AND customer.name LIKE %:name%"
+        """
+        SELECT customer FROM Customer customer 
+                LEFT JOIN FETCH customer.tenant 
+                WHERE customer.tenantId = :tenantId 
+                AND customer.name LIKE %:name%
+                """
     )
     fun findByNameContainingAndTenantId(@Param("name") name: String, @Param("tenantId") tenantId: Long): List<Customer>;
 }
