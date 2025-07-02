3724137801b21b9313fe54464889eed7ecbe1c74
Rohi <roohinofrsti@gmail.com>
2025-07-02 12:48:24 +0400

    add tenant module

--- customer/build.gradle.kts
+++ customer/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 dependencies {
     implementation(project(":company-lookup"))
     implementation(project(":util"))
+    implementation(project(":tenant"))
 
     implementation("org.springframework:spring-context")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
