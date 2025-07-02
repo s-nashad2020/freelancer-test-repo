20818a9641bcb6fc684bc0e7bb6c33917e1b9ef3
Rohi <roohinofrsti@gmail.com>
2025-07-02 12:26:05 +0400

    add util module

--- customer/build.gradle.kts
+++ customer/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 
 dependencies {
     implementation(project(":company-lookup"))
+    implementation(project(":util"))
 
     implementation("org.springframework:spring-context")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
