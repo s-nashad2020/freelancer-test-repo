1b9a4b5e0afe9694a6a56a4f231a48e3c5d46d6a
Alireza Beygi <94733363+alirezabeygijelva@users.noreply.github.com>
2025-07-04 08:32:00 +0300

    25: Add object to map

--- web-app/src/main/kotlin/com/respiroc/webapp/controller/BaseController.kt
+++ web-app/src/main/kotlin/com/respiroc/webapp/controller/BaseController.kt
@@ -3,6 +3,7 @@ package com.respiroc.webapp.controller
 import com.respiroc.util.context.UserContext
 import com.respiroc.util.context.SpringUser
 import org.springframework.security.core.context.SecurityContextHolder
+import kotlin.reflect.full.memberProperties
 
 open class BaseController {
 
@@ -12,5 +13,11 @@ open class BaseController {
 
     fun user(): UserContext {
         return springUser().ctx
+    }
+
+    inline fun <reified T : Any> toMap(obj: T): Map<String, Any?> {
+        return T::class.memberProperties.associate { prop ->
+            prop.name to prop.get(obj)
+        }
     }
 }
