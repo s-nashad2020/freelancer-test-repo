f0f5fe645ddaebeee38259910ef1453b8ad12045
Rohi <roohinofrsti@gmail.com>
2025-07-05 16:06:10 +0400

    add Person/Company

--- customer/src/main/kotlin/com/respiroc/customer/domain/model/CustomerType.kt
+++ customer/src/main/kotlin/com/respiroc/customer/domain/model/CustomerType.kt
@@ -1,7 +1,7 @@
 package com.respiroc.customer.domain.model
 
 enum class CustomerType(val type: String) {
-    CUSTOMER("CUSTOMER"),
-    SUPPLIER("SUPPLIER"),
-    CUSTOMER_SUPPLIER("CUSTOMER/SUPPLIER")
+    CUSTOMER("customer"),
+    SUPPLIER("supplier"),
+    CUSTOMER_SUPPLIER("customer/supplier")
 }
