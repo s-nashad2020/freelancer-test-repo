a58e26dafc1693b393726f16fff2d90fa4c36e61
Rohi <roohinofrsti@gmail.com>
2025-07-14 16:23:55 +0400

    normalize address value field

--- address/src/main/kotlin/com/respiroc/address/application/AddressService.kt
+++ address/src/main/kotlin/com/respiroc/address/application/AddressService.kt
@@ -8,17 +8,34 @@ import org.springframework.stereotype.Service
 @Service
 class AddressService(val addressRepository: AddressRepository) {
     fun getOrCreateAddress(payload: CreateAddressPayload): Address {
-        return addressRepository.upsertAddress(createAddress(payload))
+        return addressRepository.upsertAddress(createNormalizeAddress(payload))
     }
 
-    private fun createAddress(payload: CreateAddressPayload): Address {
+    private fun createNormalizeAddress(payload: CreateAddressPayload): Address {
         val address = Address()
-        address.countryIsoCode = payload.countryIsoCode
-        address.city = payload.city
-        address.postalCode = payload.postalCode
-        address.administrativeDivisionCode = payload.administrativeDivisionCode
-        address.primaryAddress = payload.primaryAddress
-        address.secondaryAddress = payload.secondaryAddress
+        address.countryIsoCode = payload.countryIsoCode.uppercase()
+        address.city = normalizeAddressField(payload.city)!!
+        address.postalCode = removeSpaces(payload.postalCode)
+        address.administrativeDivisionCode = removeSpaces(payload.administrativeDivisionCode)?.uppercase()
+        address.primaryAddress = normalizeAddressField(payload.primaryAddress)!!
+        address.secondaryAddress = normalizeAddressField(payload.secondaryAddress)
         return address
+    }
+
+    fun normalizeAddressField(input: String?): String? {
+        if (input == null) return null
+        val cleaned = removeSpaces(input, " ")!!
+        if (cleaned.isEmpty()) return cleaned
+        return cleaned
+            .lowercase()
+            .split(" ")
+            .filter { it.isNotEmpty() }
+            .joinToString(" ") { word -> word.replaceFirstChar { it.uppercaseChar() } }
+    }
+
+    fun removeSpaces(input: String?, replacement: String = ""): String? {
+        return input
+            ?.trim()
+            ?.replace(Regex("\\s+"), replacement)
     }
 }
