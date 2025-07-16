c2199fdfe3c91f552131e308245cec7dc9964007
moussaelaq <moussa.el39@gmail.com>
2025-07-16 20:00:32 +0100

    :construction: fix navigation, styling and optimize imports

--- ledger/src/main/kotlin/com/respiroc/ledger/application/LedgerService.kt
+++ ledger/src/main/kotlin/com/respiroc/ledger/application/LedgerService.kt
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-import com.respiroc.ledger.api.LedgerApi
 import com.respiroc.ledger.domain.model.ChartOfAccount
 import org.springframework.stereotype.Service
 
