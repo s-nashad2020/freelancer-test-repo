419e905b7e5aa819ae97eccc3e48f0a12cce1ae1
gregt <gregjotau@gmail.com>
2025-07-16 12:19:29 +0200

    clean

---
+++ web-app/src/main/kotlin/com/respiroc/webapp/controller/rest/Attachment.kt
@@ -0,0 +1,31 @@
+package com.respiroc.webapp.controller.rest
+
+import com.respiroc.util.repository.CustomJpaRepository
+import jakarta.persistence.*
+import org.hibernate.annotations.CreationTimestamp
+import org.springframework.stereotype.Repository
+import java.time.Instant
+
+@Entity
+@Table(name = "attachments")
+class Attachment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
+
+    @Column(name = "file_data", nullable = false, columnDefinition = "BYTEA")
+    lateinit var fileData: ByteArray
+
+    @Column(name = "filename", nullable = false)
+    lateinit var filename: String
+
+    @Column(name = "mimetype", nullable = false)
+    lateinit var mimetype: String
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    var createdAt: Instant? = null
+}
+
+@Repository
+interface AttachmentRepository : CustomJpaRepository<Attachment, Long>
--- web-app/src/main/kotlin/com/respiroc/webapp/controller/rest/VoucherReception.kt
+++ web-app/src/main/kotlin/com/respiroc/webapp/controller/rest/VoucherReception.kt
@@ -131,28 +131,3 @@ class VoucherReceptionDocument {
     @JoinColumn(name = "tenant_id", nullable = false)
     lateinit var tenant: Tenant
 }
-
-
-@Entity
-@Table(name = "attachments")
-class Attachment {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long? = null
-
-    @Column(name = "file_data", nullable = false, columnDefinition = "BYTEA")
-    lateinit var fileData: ByteArray
-
-    @Column(name = "filename", nullable = false)
-    lateinit var filename: String
-
-    @Column(name = "mimetype", nullable = false)
-    lateinit var mimetype: String
-
-    @CreationTimestamp
-    @Column(name = "created_at", nullable = false)
-    var createdAt: Instant? = null
-}
-
-@Repository
-interface AttachmentRepository : CustomJpaRepository<Attachment, Long>
