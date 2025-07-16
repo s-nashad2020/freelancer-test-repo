32cb156c6aefa7fbbdf82aea21cee8258d7195b3
moussaelaq <moussa.el39@gmail.com>
2025-07-16 19:16:03 +0100

    :heavy_plus_sign: integrate jackson.dataformat to parse YAML files

--- ledger/build.gradle.kts
+++ ledger/build.gradle.kts
@@ -9,6 +9,7 @@ dependencies {
     implementation("org.springframework:spring-context")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.yaml:snakeyaml")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
 
     runtimeOnly("org.postgresql:postgresql")
 }
