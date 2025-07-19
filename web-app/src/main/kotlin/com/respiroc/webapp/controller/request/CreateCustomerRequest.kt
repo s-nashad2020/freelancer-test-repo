4d646c1477fc5c1c486e823fde88fcb413799094
Rohi <roohinofrsti@gmail.com>
2025-07-19 15:04:57 +0400

    add toPayload function

--- web-app/src/main/kotlin/com/respiroc/webapp/controller/request/CreateCustomerRequest.kt
+++ web-app/src/main/kotlin/com/respiroc/webapp/controller/request/CreateCustomerRequest.kt
@@ -1,5 +1,6 @@
 package com.respiroc.webapp.controller.request
 
+import com.respiroc.common.payload.NewContactPayload
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Size
 
@@ -24,3 +25,17 @@ data class CreateCustomerRequest(
     val addressPart1: String? = "",
     val addressPart2: String? = "",
 )
+
+fun CreateCustomerRequest.toPayload(): NewContactPayload {
+    return NewContactPayload(
+        name = this.name,
+        organizationNumber = this.organizationNumber,
+        privateContact = this.privateContact,
+        countryCode = this.countryCode,
+        postalCode = this.postalCode,
+        addressPart1 = this.addressPart1,
+        addressPart2 = this.addressPart2,
+        city = this.city,
+        administrativeDivisionCode = this.administrativeDivisionCode
+    )
+}
