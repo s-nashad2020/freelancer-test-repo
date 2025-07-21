7635c4bbe44cfb3dac475bbea726322f92cf90c9
khan <khanhdn.vtb@gmail.com>
2025-07-21 17:16:39 +0700

    make shortcut logic re-useable for all page

--- web-app/src/main/resources/static/assets/base/base.js
+++ web-app/src/main/resources/static/assets/base/base.js
@@ -43,6 +43,8 @@ document.addEventListener('DOMContentLoaded', function () {
     // Handle section anchors
     const sectionAnchors = document.querySelectorAll("[slot*='navigation'] a[href*='#']");
     sectionAnchors.forEach(sectionAnchor => sectionAnchor.setAttribute('data-drawer', 'close'));
+
+    document.addEventListener("keydown", handleShortcutEvent);
 });
 
 // Handle aside visibility based on content
@@ -79,3 +81,34 @@ document.addEventListener('click', function(e) {
         }
     }
 });
+
+function handleShortcutEvent(event) {
+    if (shortcutAction && shortcutAction.length > 0) {
+        const combo = normalizeKeyCombination(event);
+
+        const matchedShortcut = shortcutAction.find(
+            s => s.keyCombination.toLowerCase() === combo
+        );
+
+        if (matchedShortcut) {
+            event.preventDefault();
+            const action = shortcutMap[matchedShortcut.actionId];
+            if (action) {
+                action();
+            } else {
+                console.warn("No handler for actionId:", matchedShortcut.actionId);
+            }
+        }
+    }
+}
+
+function normalizeKeyCombination(event) {
+    const keys = [];
+    if (event.ctrlKey) keys.push("ctrl");
+    if (event.altKey) keys.push("alt");
+    if (event.shiftKey) keys.push("shift");
+    const key = event.key.toLowerCase();
+    keys.push(key);
+    return keys.join("+");
+}
+
