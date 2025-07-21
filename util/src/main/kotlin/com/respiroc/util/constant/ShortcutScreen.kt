051cb6acb96469d9ccd79ce621dd5418caf96003
khan <khanhdn.vtb@gmail.com>
2025-07-21 17:15:17 +0700

    add data structure to store shortcut

---
+++ util/src/main/kotlin/com/respiroc/util/constant/ShortcutScreen.kt
@@ -0,0 +1,6 @@
+package com.respiroc.util.constant
+
+enum class ShortcutScreen(val displayName: String) {
+    VOUCHERS_ADVANCED("Advanced voucher screen"),
+    VOUCHERS_OVERVIEW("Overview voucher screen"),
+}
